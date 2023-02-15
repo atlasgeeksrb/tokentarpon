fc277a6920667e2fb996826f839a51cb98e1fbc5
atlasgeeksrb <rb@atlasgeekservices.com>
2023-02-15 09:37:32 -0500

    Initial token tests and module

---
+++ token/token.go
@@ -0,0 +1,56 @@
+// handles token CRD
+package token
+
+import (
+	"errors"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var DefaultMaxLength int = 255
+
+var (
+	ErrEmptyValue      = errors.New("cannot store empty value")
+	ErrValueTooBig     = errors.New("value too large for storage")
+	ErrNoMatchingToken = errors.New("no token found for provided domain and token id")
+)
+
+type Token struct {
+	Uuid           string `bson:"uuid" json:"uuid"`
+	DomainUuid     string `bson:"domainUuid" json:"domainUuid"`
+	Value          string `bson:"value" json:"value"`
+	EncryptedValue string `bson:"encryptedValue" json:"encryptedValue"`
+	Created        int64  `json:"created" bson:"created"`
+	IsDeleted      bool   `json:"isdeleted" bson:"isdeleted"`
+}
+
+func CreateToken(domainUuid string, value string) (Token, error) {
+	var tok Token
+
+	err := errors.New("data: token incomplete, need domain id, value")
+	if len(strings.TrimSpace(domainUuid)) == 0 {
+		return tok, err
+	}
+	if len(strings.TrimSpace(value)) == 0 {
+		return tok, err
+	}
+	aUuid := uuid.New()
+	tok.DomainUuid = domainUuid
+	tok.Value = value
+	tok.Uuid = aUuid.String()
+	tok.Created = time.Now().Unix()
+
+	//@todo store token
+	return tok, nil
+}
+
+func GetToken(domainUuid string, tokenUuid string) (Token, error) {
+	var tok Token
+	return tok, nil
+}
+
+func DeleteToken(domainuuid string, tokenUuid string) error {
+	return nil
+}
---
+++ token/token_test.go
@@ -0,0 +1,81 @@
+package token
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+)
+
+func TestCreateToken(t *testing.T) {
+	testScenarios := []struct {
+		givenValue, givenDomain string
+		expectedErrorMsg        string
+	}{
+		{
+			givenValue:  "this is a perfectly acceptable string to be tokenized",
+			givenDomain: "mydomain",
+		},
+		{
+			givenValue:       "",
+			givenDomain:      "mydomain",
+			expectedErrorMsg: ErrEmptyValue.Error(),
+		},
+		{
+			givenValue:       "oh my, this message is bigger than the max 01234567890 - oh my, this message is bigger than the max 01234567890 - oh my, this message is bigger than the max 01234567890 - oh my, this message is bigger than the max 01234567890 - oh my, this message is bigger than the max 01234567890 - oh my, this message is bigger than the max 01234567890",
+			givenDomain:      "mydomain",
+			expectedErrorMsg: ErrValueTooBig.Error(),
+		},
+	}
+
+	for i, scenario := range testScenarios {
+
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+
+			// 1./2. given/do
+			tok, createerr := CreateToken(scenario.givenDomain, scenario.givenValue)
+
+			// 3.1 expect error message
+			if nil != createerr && len(scenario.expectedErrorMsg) > 0 && scenario.expectedErrorMsg != createerr.Error() {
+				t.Error(createerr.Error())
+				return
+			}
+
+			// 3.2 try get normally
+			createdtoken, geterr := GetToken(scenario.givenDomain, tok.Uuid)
+			if nil != geterr {
+				t.Error(geterr.Error())
+				return
+			} else if want, got := scenario.givenValue, createdtoken.Value; want != got {
+				t.Error(fmt.Printf("expect token value %#v but got %#v", want, got))
+				return
+			}
+
+			// 3.3 try get with wrong domain
+			_, geterr2 := GetToken("thisisnotmybeautifuldomain", tok.Uuid)
+			if want, got := ErrNoMatchingToken, geterr2; want != got {
+				t.Error(fmt.Printf("expect error %#v but got %#v", want, got))
+			}
+
+			// 3.3 try delete
+			deleteerr := DeleteToken(tok.DomainUuid, tok.Uuid)
+			if nil != deleteerr {
+				t.Error(deleteerr.Error())
+			}
+
+			// 3.4 try delete token that doesn't exist
+			deleteerr2 := DeleteToken("thisisnotmybeautifuldomain", tok.Uuid)
+			if want, got := ErrNoMatchingToken, deleteerr2; want != got {
+				t.Error(fmt.Printf("expect error %#v but got %#v", want, got))
+			}
+
+		})
+	}
+}
+
+func TestDeleteToken(t *testing.T) {
+	t.Fatal("argggh")
+}
+
+func TestGetToken(t *testing.T) {
+	t.Fatal("argggh")
+}
