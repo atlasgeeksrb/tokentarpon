411b3349529e4aba58bee21a520d32563bd4f2e4
atlasgeeksrb <rb@atlasgeekservices.com>
2023-02-16 11:33:47 -0500

    Perform CRUD operations using datastore package

--- tokenizer/tokenizer.go
+++ tokenizer/tokenizer.go
@@ -2,13 +2,55 @@ package tokenizer
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 	"time"
+	"tokentarpon/crypto"
+	"tokentarpon/tokenizer/datastore"
 
 	"github.com/google/uuid"
 )
 
 var DefaultMaxLength int = 255
+var MaxRecords int64 = 100
+var UnitTest = false
+var tokenRecordType = "token"
+
+type Token struct {
+	Uuid           string `bson:"uuid" json:"uuid"`
+	DomainUuid     string `bson:"domainUuid" json:"domainUuid"`
+	Value          string `bson:"value" json:"value"`
+	EncryptedValue string `bson:"encryptedValue" json:"encryptedValue"`
+	IsDeleted      bool   `json:"isdeleted" bson:"isdeleted"`
+	DocumentType   string `bson:"documentType" json:"documentType"`
+	Version        string `bson:"version" json:"version"`
+	Created        int64  `json:"created" bson:"created"`
+	Updated        int64  `json:"updated" bson:"updated"`
+	Check          string `bson:"check" json:"check"`
+}
+
+type Token_v001 struct {
+	Uuid           string `bson:"uuid" json:"uuid"`
+	DomainUuid     string `bson:"domainUuid" json:"domainUuid"`
+	Value          string `bson:"value" json:"value"`
+	EncryptedValue string `bson:"encryptedValue" json:"encryptedValue"`
+	IsDeleted      bool   `json:"isdeleted" bson:"isdeleted"`
+	DocumentType   string `bson:"documentType" json:"documentType"`
+	Version        string `bson:"version" json:"version"`
+	Created        int64  `json:"created" bson:"created"`
+	Updated        int64  `json:"updated" bson:"updated"`
+	Check          string `bson:"check" json:"check"`
+}
+
+type TokenQuery struct {
+	DomainUuid string   `bson:"domainUuid" json:"domainUuid"`
+	Uuids      []string `bson:"uuids" json:"uuids"`
+}
+
+type TokenError struct {
+	Token Token  `bson:"token" json:"token"`
+	Error string `bson:"error" json:"error"`
+}
 
 var (
 	ErrEmptyValue      = errors.New("cannot store empty value")
@@ -16,16 +58,7 @@ var (
 	ErrNoMatchingToken = errors.New("no token found for provided domain and token id")
 )
 
-type Token struct {
-	Uuid           string `bson:"uuid" json:"uuid"`
-	DomainUuid     string `bson:"domainUuid" json:"domainUuid"`
-	Value          string `bson:"value" json:"value"`
-	EncryptedValue string `bson:"encryptedValue" json:"encryptedValue"`
-	Created        int64  `json:"created" bson:"created"`
-	IsDeleted      bool   `json:"isdeleted" bson:"isdeleted"`
-}
-
-func CreateToken(domainUuid string, value string) (Token, error) {
+func CreateToken(collectionName string, domainUuid string, value string) (Token, error) {
 	var tok Token
 
 	err := errors.New("data: token incomplete, need domain id, value")
@@ -41,15 +74,222 @@ func CreateToken(domainUuid string, value string) (Token, error) {
 	tok.Uuid = aUuid.String()
 	tok.Created = time.Now().Unix()
 
-	//@todo store token
-	return tok, nil
+	if UnitTest {
+		return tok, nil
+	}
+
+	datastore.CollectionName = collectionName
+	result, errInsert := datastore.InsertRecord(tokenRecordType, tok)
+	if errInsert != nil {
+		return tok, errInsert
+	} else if nil == result {
+		return tok, datastore.ErrNotFound
+	} else {
+		insertedToken := result.(Token)
+		return insertedToken, nil
+	}
 }
 
-func GetToken(domainUuid string, tokenUuid string) (Token, error) {
+func CreateTokens(collectionName, domainUuid string, tokens []Token) ([]Token, []TokenError) {
+	var createdTokens []Token
+	var errorTokens []TokenError
+	datastore.CollectionName = collectionName
+
+	for _, tokenObj := range tokens {
+		if len(strings.TrimSpace(tokenObj.DomainUuid)) == 0 {
+			e := TokenError{Token: tokenObj, Error: "Missing Domain ID"}
+			errorTokens = append(errorTokens, e)
+		} else if tokenObj.DomainUuid != domainUuid {
+			e := TokenError{Token: tokenObj, Error: "Invalid Domain ID"}
+			errorTokens = append(errorTokens, e)
+		} else if len(strings.TrimSpace(tokenObj.Value)) == 0 {
+			e := TokenError{Token: tokenObj, Error: "Missing Token Value"}
+			errorTokens = append(errorTokens, e)
+		} else {
+			aUuid := uuid.New()
+			tokenObj.Uuid = aUuid.String()
+			tokenObj.Created = time.Now().Unix()
+
+			if UnitTest {
+				createdTokens = append(createdTokens, tokenObj)
+			} else {
+				result, errInsert := datastore.InsertRecord(tokenRecordType, tokenObj)
+				if errInsert != nil {
+					errmsg := fmt.Sprint(errInsert)
+					e := TokenError{Token: tokenObj, Error: errmsg}
+					errorTokens = append(errorTokens, e)
+				} else if nil == result {
+					e := TokenError{Token: tokenObj, Error: "The token was inserted but not returned"}
+					errorTokens = append(errorTokens, e)
+				} else {
+					tok := result.(Token)
+					createdTokens = append(createdTokens, tok)
+				}
+			}
+		}
+	}
+	return createdTokens, errorTokens
+}
+
+func GetToken(collectionName string, domainUuid string, tokenUuid string) (Token, error) {
 	var tok Token
-	return tok, nil
+	err := errors.New("data: need domain id, token id")
+	if len(strings.TrimSpace(domainUuid)) == 0 {
+		return tok, err
+	}
+	if len(strings.TrimSpace(tokenUuid)) == 0 {
+		return tok, err
+	}
+
+	if UnitTest {
+		tok.DomainUuid = domainUuid
+		tok.Uuid = tokenUuid
+		return tok, nil
+	}
+
+	filter := datastore.MakeDomainQuery(domainUuid, "uuid", tokenUuid, true)
+	datastore.CollectionName = collectionName
+	result, err := datastore.GetRecord(tokenRecordType, filter)
+	if err == nil {
+		tok = result.(Token)
+	}
+	return tok, err
 }
 
-func DeleteToken(domainuuid string, tokenUuid string) error {
-	return nil
+func DeleteToken(collectionName string, tokenObj Token) (Token, error) {
+	var empty Token
+	err := errors.New("data: token incomplete, need domain id, token id, value")
+	if len(strings.TrimSpace(tokenObj.DomainUuid)) == 0 {
+		return empty, err
+	}
+	if len(strings.TrimSpace(tokenObj.Uuid)) == 0 {
+		return empty, err
+	}
+
+	if UnitTest {
+		tokenObj.IsDeleted = true
+		return tokenObj, nil
+	}
+
+	filter := datastore.MakeDomainQuery(tokenObj.DomainUuid, "uuid", tokenObj.Uuid, false)
+	datastore.CollectionName = collectionName
+	result, err := datastore.GetRecord(tokenRecordType, filter)
+	if err != nil {
+		return empty, err
+	}
+
+	var t Token = result.(Token)
+	t.IsDeleted = true
+	updateResult, err := datastore.UpdateRecord(tokenRecordType, filter, "and", t)
+	updatedToken := updateResult.(Token)
+	return updatedToken, err
+}
+
+func CreateMultiTokenQuery(tokenQuery TokenQuery) []datastore.DataQueryGroup {
+
+	var filters = make([]datastore.DataQueryGroup, 2)
+	var nvq datastore.DataQueryGroup
+
+	nvq.Operator = "and"
+	nvq.DataQueries = make([]datastore.DataQuery, 1)
+	nvq.DataQueries[0].FieldName = "domainUuid"
+	nvq.DataQueries[0].FieldValue = tokenQuery.DomainUuid
+	nvq.DataQueries[0].Wildcard = false
+	nvq.DataQueries[0].CaseSensitive = true
+	filters[0] = nvq
+
+	nvq.Operator = "or"
+	nvq.DataQueries = make([]datastore.DataQuery, len(tokenQuery.Uuids))
+	for idx, uuid := range tokenQuery.Uuids {
+		//fmt.Println("Slice item", i, "is", numbers[i])
+		nvq.DataQueries[idx].FieldName = "uuid"
+		nvq.DataQueries[idx].FieldValue = uuid
+		nvq.DataQueries[idx].Wildcard = false
+		nvq.DataQueries[idx].CaseSensitive = true
+	}
+	filters[1] = nvq
+	return filters
+}
+
+func GetTokens(collectionName string, domainUuid string) ([]Token, error) {
+	var empty []Token
+	err := errors.New("data: need domain id")
+	if len(strings.TrimSpace(domainUuid)) == 0 {
+		return empty, err
+	}
+
+	if UnitTest {
+		return empty, err
+	}
+
+	datastore.CollectionName = collectionName
+	filter := datastore.MakeSimpleQuery("domainUuid", domainUuid, false)
+	results, err := datastore.GetRecords(tokenRecordType, filter, "and", 0, 100)
+	if err != nil {
+		return nil, datastore.ErrQueryError
+	} else {
+		tokens := make([]Token, 0)
+		for _, x := range results {
+			tokens = append(tokens, x.(Token))
+		}
+		return tokens, nil
+	}
+}
+
+func GetTokenValues(collectionName string, tokenQuery TokenQuery) ([]string, error) {
+	var empty []string
+	err := errors.New("data: need domain id")
+	if len(strings.TrimSpace(tokenQuery.DomainUuid)) == 0 {
+		return empty, err
+	}
+	err = errors.New("data: need uuids")
+	if len(tokenQuery.Uuids) == 0 {
+		return empty, err
+	}
+
+	if UnitTest {
+		return empty, err
+	}
+
+	filter := CreateMultiTokenQuery(tokenQuery)
+	datastore.CollectionName = collectionName
+	results, err := datastore.GetRecords(tokenRecordType, filter, "and", 0, MaxRecords)
+	if err != nil {
+		return nil, datastore.ErrQueryError
+	} else {
+		tokenValues := make([]string, 0)
+
+		// return the token values at the same indices as their uuids
+		for _, uuid := range tokenQuery.Uuids {
+			for _, tv := range results {
+				if uuid == tv.(Token).Uuid {
+					tokenValues = append(tokenValues, tv.(Token).Value)
+				}
+			}
+		}
+		return tokenValues, nil
+	}
+
+}
+
+func EncryptValues(plaintext []string, key string) []string {
+	encryptedValues := make([]string, 0)
+	for _, plain := range plaintext {
+		encrypted, err := crypto.EncryptAES(plain, key)
+		if nil == err {
+			encryptedValues = append(encryptedValues, encrypted)
+		}
+	}
+	return encryptedValues
+}
+
+func DecryptValues(encryptedValues []string, key string) []string {
+	plaintext := make([]string, 0)
+	for _, enc := range encryptedValues {
+		encrypted, err := crypto.DecryptAES(enc, key)
+		if nil == err {
+			plaintext = append(plaintext, encrypted)
+		}
+	}
+	return plaintext
 }
--- tokenizer/tokenizer_test.go
+++ tokenizer/tokenizer_test.go
@@ -27,6 +27,7 @@ func TestCreateToken(t *testing.T) {
 		},
 	}
 
+	Test = true
 	for i, scenario := range testScenarios {
 
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
@@ -73,9 +74,209 @@ func TestCreateToken(t *testing.T) {
 }
 
 func TestDeleteToken(t *testing.T) {
+	Test = true
 	t.Fatal("argggh")
 }
 
 func TestGetToken(t *testing.T) {
+	Test = true
 	t.Fatal("argggh")
 }
+
+/*
+// test fixtures
+func TestAdd(t *testing.T) {
+	// go test sets pwd as package being tested
+	// my_folder is a relative path; consider using test_fixtures
+	// use it to load config, model data, binary data etc.
+	data := filepath.Join("my_folder", "add_data.json")
+
+	// do stuff with data
+
+	// golden files
+	// compare a file output to a golden file - expected file output
+	// simple use a .golden extension for a file
+	// then
+	for _, tc := range testcases {
+		golden := filepath.Join("my_folder", "add_data.json")
+
+		expected, _ := ioutil.Readfile(golden)
+		if !bytes.Equal(actual, expected) {
+			// fail
+		}
+
+	}
+
+	// test helper functions should never return an error
+	// just fail the test if there's an error
+	// see t.helper
+
+}
+
+// over-parameterize structs, for us in testing
+type ServerOpts struct {
+	CachePath string
+	Port int
+
+	Test bool
+	// consider having a test bool
+	// use this for testing;
+	// for example production code would
+	// test this value and if set to true,
+	// skip authentication
+}
+
+// testing sub-processes, options:
+// 1. test the actual process
+// in this case guard against the process not being available i.e. is git here?
+
+// 2. mock the subprocess
+// make the *exec.Cmd configurable, pass in a custom one
+// let the caller modify it
+func helperProcess (s ...string) *exec.Cmd {
+	cs := []string{"-test.run=TestHelperProcess", "--"}
+	cs.append(cs, s...)
+	env := []string{
+		"GO_WANT_HELPER_PROCESS=1",
+	}
+
+	cmd := exec.Command(os.Args[0], cs...)
+	cmd.Env = append(env, os.Environ()...)
+	return cmd
+}
+func TestHelperProcess(*testing.T) {
+	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
+		return
+	}
+	defer os.Exit(0)
+
+	args := os.Args
+	for len(args) > 0 {
+		if args[0] == "--" {
+			args = args[1:]
+			break
+		}
+
+		args = args[1:]
+	}
+}
+
+// include testing.go or testing_*.go files
+// exported APIs that provide mocks, test harnesses, helpers
+// allows other packages to test using this package,
+// without reinventing components needed to use the package in a test
+// e.g. TestConfig(t) provides valid testing config
+// TestServer(t) (net.Addr, io.Closer) fully started in-memory server and a closer to close it
+// ...and tests for things the user of our package might create,
+// to confirm that they meet the spec required by our package
+
+// go test is a great workflow tool
+// explore this
+
+// if you have a complex pluggable system
+// you can write a custom framework
+// within go test, rather than a separate test harness
+
+
+
+func testTempFile(t *testing.T) (string, func()) {
+	t.Helper()
+
+	tf, err := ioutil.TempFile("", "test")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	tf.Close()
+
+	return tf.Name(), func() { os.Remove(tf.Name())}
+}
+
+func TestThing(t *testing.T) {
+	tf, tfclose := testTempFile(t)
+	defer tfclose()
+}
+
+// don't mock net.Conn! go ahead and create a network connection
+func TestConn(t *testing.T) (client, server net.Conn) {
+	t.Helper()
+
+	// one-connection example
+	// also easy to make a n-connection example
+	// easy to test any protocol
+	// easy to return the listener
+	// easy to test IPv6 if needed
+
+	ln, err := net.listen("tcp", "127.0.0.1:0")
+
+	var server net.Conn
+	go fnc() {
+		defer ln.Close()
+		server, err = ln.Accept()
+	}()
+
+	client, err := net.Dial("tcp", ln.Addr().String())
+	return client, server
+}
+
+func TestGetToken(t *testing.T) {
+
+	var stringValue = "Some test string"
+	token, err := GetToken("mydomain", stringValue)
+	if err != nil {
+		t.Log("error should be nil", err)
+		t.Fail()
+	}
+	if len(strings.TrimSpace(token.Uuid)) == 0 {
+		t.Log("got empty token, should not be empty", err)
+		t.Fail()
+	}
+}
+
+func TestGetTokens(t *testing.T) {
+	tokens, err := GetUsers()
+	if err != nil {
+		t.Log("error should be nil", err)
+		t.Fail()
+	}
+	if len(tokens) == 0 {
+		t.Log("no tokens found, there should be some tokens", err)
+		t.Fail()
+	}
+}
+
+func TestGetTokenValues(t *testing.T) {
+	newUser := Token{
+		Uuid:     "abc123",
+		Username: existingUsername,
+		Password: userPassword,
+		Name:     userName,
+	}
+	// expect error when we re-use a username
+	_, err := CreateUser(newUser)
+	if nil == err {
+		t.Log("error should not be nil for create user with existing name", err)
+		t.Fail()
+	}
+	newUser.Username = "superbrandnewuser"
+	token, err := CreateUser(newUser)
+	if nil != err {
+		t.Log("error should be nil for create user with new username", err)
+		t.Fail()
+	}
+	if token.Uuid == newUser.Uuid {
+		t.Log("new token uuid should be set by CreateToken function", err)
+		t.Fail()
+	}
+
+}
+func TestCreateToken(t *testing.T) {
+}
+func TestCreateTokens(t *testing.T) {
+}
+func TestDeleteToken(t *testing.T) {
+}
+func TestEncryptValues(t *testing.T) {
+}
+func TestDecryptValues(t *testing.T) {
+}
+*/
