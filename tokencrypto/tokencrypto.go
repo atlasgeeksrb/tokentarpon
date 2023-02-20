aeb207c3d6476123c77b72d2fa91eb434d6b7b29
atlasgeeksrb <rb@atlasgeekservices.com>
2023-02-20 13:21:34 -0500

    Clean up encryption functions, write tests

--- tokencrypto/tokencrypto.go
+++ tokencrypto/tokencrypto.go
@@ -2,9 +2,12 @@ package tokencrypto
 
 import (
 	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"crypto/sha1"
-	"encoding/base64"
 	"encoding/hex"
+	"errors"
+	"io"
 )
 
 func GetHashForByteArray(value []byte) string {
@@ -21,38 +24,97 @@ func GetHashForString(value string) string {
 	return sha1_hash
 }
 
-func EncryptAES(plaintext string, key string) (string, error) {
-	// create cipher
-	bkey := []byte(key)
-	c, err := aes.NewCipher(bkey)
-	if nil != err {
+// Encryption functions credit
+// https://gist.github.com/17twenty/b7a050d6a3ed991db0433d4a1fc50de7
+func EncryptAES(plaintextValue string, keyValue string) (string, error) {
+
+	// Byte array of the string
+	plaintext := []byte(plaintextValue)
+
+	// Key
+	key := []byte(keyValue)
+
+	keyError := checkKey(key)
+	if keyError != nil {
+		return "", keyError
+	}
+
+	// Create the AES cipher
+	block, err := aes.NewCipher(key)
+	if err != nil {
 		return "", err
 	}
 
-	newplain := base64.StdEncoding.EncodeToString([]byte(plaintext))
+	// Empty array of 16 + plaintext length
+	// Include the IV at the beginning
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
 
-	// allocate space for ciphered data
-	out := make([]byte, len(newplain))
+	// Slice of first 16 bytes
+	iv := ciphertext[:aes.BlockSize]
 
-	// encrypt
-	c.Encrypt(out, []byte(newplain))
+	// Write 16 rand bytes to fill iv
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
 
-	// return hex string
-	return hex.EncodeToString(out), nil
+	// Return an encrypted stream
+	stream := cipher.NewCFBEncrypter(block, iv)
+
+	// Encrypt bytes from plaintext to ciphertext
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	return string(ciphertext), nil
 }
 
-func DecryptAES(encryptedValue string, key string) (string, error) {
-	bkey := []byte(key)
-	ciphertext, _ := hex.DecodeString(encryptedValue)
+func DecryptAES(encryptedValue string, keyValue string) (string, error) {
 
-	c, err := aes.NewCipher(bkey)
-	if nil != err {
+	// Byte array of the string
+	ciphertext := []byte(encryptedValue)
+
+	// Key
+	key := []byte(keyValue)
+
+	keyError := checkKey(key)
+	if keyError != nil {
+		return "", keyError
+	}
+
+	// Create the AES cipher
+	block, err := aes.NewCipher(key)
+	if err != nil {
 		return "", err
 	}
 
-	pt := make([]byte, len(ciphertext))
-	c.Decrypt(pt, ciphertext)
+	// Before even testing the decryption,
+	// if the text is too small, then it is incorrect
+	if len(ciphertext) < aes.BlockSize {
+		tooshortErr := errors.New("text is too short")
+		return "", tooshortErr
+	}
 
-	s := string(pt[:])
-	return s, nil
+	// Get the 16 byte IV
+	iv := ciphertext[:aes.BlockSize]
+
+	// Remove the IV from the ciphertext
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	// Return a decrypted stream
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	// Decrypt bytes from ciphertext
+	stream.XORKeyStream(ciphertext, ciphertext)
+
+	return string(ciphertext), nil
+}
+
+func checkKey(key []byte) error {
+	// check key size
+	keylen := len(key)
+	if keylen != 16 && keylen != 32 && keylen != 64 {
+		badKeySize := errors.New("key size must be 16, 32 or 64")
+		return badKeySize
+	}
+	// any other checks?
+
+	return nil
 }
--- tokencrypto/tokencrypto_test.go
+++ tokencrypto/tokencrypto_test.go
@@ -1,17 +1,112 @@
-package crypto
+package tokencrypto
 
 import (
+	"encoding/json"
+	"errors"
+	"strings"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
+type Foo struct {
+	Uuid         string `bson:"uuid" json:"uuid"`
+	Value        string `bson:"value" json:"value"`
+	DocumentType string `bson:"documentType" json:"documentType"`
+	Version      string `bson:"version" json:"version"`
+	Created      int64  `json:"created" bson:"created"`
+	Updated      int64  `json:"updated" bson:"updated"`
+	Check        string `bson:"check" json:"check"`
+}
+
 func TestGetHashForByteArray(t *testing.T) {
-	t.Fatal("not implemented")
+
+	nownow := time.Now().Unix()
+	var x Foo = Foo{
+		Uuid:         "abc123",
+		Value:        "some rando value",
+		DocumentType: "Foo",
+		Version:      "001",
+		Created:      nownow,
+		Updated:      nownow,
+	}
+	j, _ := json.Marshal(x)
+	hash := GetHashForByteArray(j)
+	x.Check = hash
+
+	// hash can't be empty
+	assert.NotEqual(t, strings.TrimSpace(hash), "")
+
+	// re-hashing the same data must yield an identical hash
+	assert.Equal(t, GetHashForByteArray(j), hash)
+
+	// hashing data with slight change must yield a different hash
+	x.Updated = time.Now().Unix()
+	j, _ = json.Marshal(x)
+	assert.NotEqual(t, strings.TrimSpace(GetHashForByteArray(j)), hash)
+
 }
 
 func TestGetHashForString(t *testing.T) {
-	t.Fatal("not implemented")
+
+	teststring := "This is a delishously hashable string!"
+	hash := GetHashForString(teststring)
+
+	// hash can't be empty
+	assert.NotEqual(t, hash, "")
+
+	// re-hashing the same data must yield an identical hash
+	assert.Equal(t, GetHashForString(teststring), hash)
+
+	// hashing data with slight change must yield a different hash
+	teststring += " "
+	assert.NotEqual(t, GetHashForString(teststring), hash)
+
 }
 
-func TestEncryptString(t *testing.T) {
-	t.Fatal("not implemented")
+func TestEncryptAES(t *testing.T) {
+
+	plaintextValue := "this is some value!"
+
+	// The key argument should be the AES key,
+	// either 16, 24, or 32 bytes to select AES-128, AES-192, or AES-256.
+	key := ";kldfpo87-28374isu;dfjhZXJCVG786"
+
+	encryptedValue, encryptionErr := EncryptAES(plaintextValue, key)
+	if nil != encryptionErr {
+		t.Fatal(encryptionErr)
+	}
+
+	// encryptedValue can't be empty
+	assert.NotEqual(t, strings.TrimSpace(encryptedValue), "")
+
+	// encryptedValue can't equal plaintextValue
+	assert.NotEqual(t, strings.TrimSpace(encryptedValue), plaintextValue)
+
+	// it's hard to test encryption without confirming that decryption is also working
+	decryptedValue, decryptionErr := DecryptAES(encryptedValue, key)
+	if nil != decryptionErr {
+		t.Fatal(decryptionErr)
+	}
+
+	// decryptedValue must equal plaintextValue
+	assert.Equal(t, strings.TrimSpace(decryptedValue), plaintextValue)
+
+	// decryptedValue must not equal encryptedValue
+	assert.NotEqual(t, decryptedValue, encryptedValue)
+
+	// less happy paths
+	// test with wrong size key
+	key = "anewkey"
+	_, encryptionErr = EncryptAES(plaintextValue, key)
+	assert.Equal(t, encryptionErr, errors.New("key size must be 16, 32 or 64"))
+
+	//@todo
+	// what's the largest string we can encrypt? should know this, and test against it
+
+}
+
+func TestDecryptAES(t *testing.T) {
+	t.Skip()
 }
